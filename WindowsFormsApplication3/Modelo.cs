bf29a88a69dacce856c591cf07019e4c35371666
Juan Giraldo <juanferwar@gmail.com>
2018-08-12 13:51:01 -0500

    Se agrego el metodo de la ingenieria

--- WindowsFormsApplication3/Modelo.cs
+++ WindowsFormsApplication3/Modelo.cs
@@ -23,7 +23,7 @@ namespace Tarea1
         }
         public void cargarUsuarios() {
         
-            String pathUsuarios = @"E:\Repos\SistemaReporte\WindowsFormsApplication3\Usuarios.txt";
+            String pathUsuarios = @"C:\Users\juan-\Source\Repos\SistemaReporte\WindowsFormsApplication3\Usuarios.txt";
             List<String> lines = File.ReadAllLines(pathUsuarios).ToList();
             foreach (string item in lines) {
                 String[] split = item.Split('-');
