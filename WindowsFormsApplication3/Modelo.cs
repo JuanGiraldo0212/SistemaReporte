92408160d0ba3f90bb087e4837fdc6cdbb57d683
David99bo <david.bolivar@correo.icesi.edu.co>
2018-08-12 15:20:27 -0500

    Se agregaron los puntos del 4 al 6 en el metodo de la ingenieria, y se agregaron los requerimentos funcionales del punto 1 del mismo metodo

--- WindowsFormsApplication3/Modelo.cs
+++ WindowsFormsApplication3/Modelo.cs
@@ -23,7 +23,7 @@ namespace Tarea1
         }
         public void cargarUsuarios() {
         
-            String pathUsuarios = @"C:\Users\juan-\Source\Repos\SistemaReporte\WindowsFormsApplication3\Usuarios.txt";
+            String pathUsuarios = @"E:\RepositoriosGit\SistemaReportes\SistemaReporte\WindowsFormsApplication3\Usuarios.txt";
             List<String> lines = File.ReadAllLines(pathUsuarios).ToList();
             foreach (string item in lines) {
                 String[] split = item.Split('-');
